190d38b30517aeaf495d0029f9cb6772f6222a98
anvikiran <134178652+anvikiran@users.noreply.github.com>
2025-01-19 17:02:40 -0800

    1/19

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/SpeciAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/SpeciAuto.java
@@ -1,6 +1,7 @@
 
 package org.firstinspires.ftc.teamcode.Auto;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
@@ -13,42 +14,46 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.MecanumDriveSWBot;
 import org.firstinspires.ftc.teamcode.mechanisms.Claw;
 import org.firstinspires.ftc.teamcode.mechanisms.ExtendoV2;
 import org.firstinspires.ftc.teamcode.mechanisms.Intaker;
 import org.firstinspires.ftc.teamcode.mechanisms.SlidesV2;
 
 @Autonomous
+@Config
 public class SpeciAuto extends LinearOpMode {
-    public static double speciDropX = -28.42;
-    public static double speciDropY = -6.61;
+
+    public static double speciDropX = -29.3;
+    public static double speciDropY = -7.74;
     public static double speciDropH = 0;
-    public static double block1X = -21.85;
-    public static double block1Y = 18.47;
-    public static double block1H = 125;
-    public static double depositHumanH = 90;
-    public static double block2X = -22.65;
-    public static double block2Y = 27.62;
-    public static double block2H = 125;
+    public static double block1X = -18.3;
+    public static double block1Y = 23.3;
+    public static double block1H = 135;
+    public static double depositHumanH = 15;
+    public static double block2X = -19;
+    public static double block2Y = 31;
+    public static double block2H = 135;
     public static double depositHumanH2 = 90;
-    public static double pickUpX = -8;
-    public static double pickUpY = 30;
-    public static double pickUpH = 180;
-    public static double dropSpecimenX = -28.42;
-    public static double dropSpecimenY =-6.61 ;
+    public static double pickUpX = -3.8;
+    public static double pickUpY = 42;
+    public static double pickUpH = -180;
+    public static double dropSpecimenX = -29.3;
+    public static double dropSpecimenY =-7.74 ;
     public static double dropSpecimenH =0;
     public static double parkX = -6;
     public static double parkY = 32;
     public static double parkH = 180;
+
     @Override
     public void runOpMode() {
 
         Pose2d StartPose1 = new Pose2d(0,0, Math.toRadians(0));
-        MecanumDrive drive = new MecanumDrive(hardwareMap, StartPose1);
-        SlidesV2 slides = new SlidesV2(hardwareMap, true);
-        ExtendoV2 extendo = new ExtendoV2(hardwareMap);
-        Claw claw = new Claw(hardwareMap);
-        Intaker intake = new Intaker(hardwareMap);
+        MecanumDriveSWBot drive = new MecanumDriveSWBot(hardwareMap, StartPose1);
+        //SlidesV2 slides = new SlidesV2(hardwareMap, true);
+        //ExtendoV2 extendo = new ExtendoV2(hardwareMap);
+        //Claw claw = new Claw(hardwareMap);
+        //Intaker intake = new Intaker(hardwareMap);
 
 
         TrajectoryActionBuilder pathT = drive.actionBuilder(StartPose1)
@@ -56,11 +61,11 @@ public class SpeciAuto extends LinearOpMode {
                 .waitSeconds(2)
                 .strafeToLinearHeading(new Vector2d(block1X, block1Y), Math.toRadians(block1H))
                 .waitSeconds(0.5)
-                .turn(depositHumanH)
+                .strafeToLinearHeading(new Vector2d(block1X, block1Y), Math.toRadians(depositHumanH))
                 .waitSeconds(1.9)
                 .strafeToLinearHeading(new Vector2d(block2X, block2Y), Math.toRadians(block2H))
                 .waitSeconds(1.9)
-                .turn(depositHumanH2)
+                .strafeToLinearHeading(new Vector2d(block1X, block1Y), Math.toRadians(depositHumanH))
                 .strafeToLinearHeading(new Vector2d(pickUpX, pickUpY), Math.toRadians(pickUpH))
                 .waitSeconds(0.7)
                 .strafeToLinearHeading(new Vector2d(dropSpecimenX, dropSpecimenY), Math.toRadians(dropSpecimenH))
@@ -82,54 +87,54 @@ public class SpeciAuto extends LinearOpMode {
         Action path = pathT.build();
 
         Actions.runBlocking(new ParallelAction(
-                path,
+                path
 
-                new SequentialAction(
-                        slides.slideTopBar(),
-                        slides.slideTopBarClip(),
-                        new SleepAction(0.25),
-                        claw.open(),
-                        new SleepAction(0.2),
-                        slides.retract(),
-                        new ParallelAction(
-
-                                extendo.extend()
-                        ),
-                        intake.flat(),
-                        new ParallelAction(
-                                extendo.retract(),
-                                intake.flop()
-                        ),
-                        claw.close(),
-                        new ParallelAction(
-                                slides.slideTopBar()
-                        ),
-                        slides.slideTopBarClip(),
-                        new SleepAction(0.25),
-                        claw.open(),
-                        new SleepAction(0.2),
-                        slides.retract(),
-                        claw.close(),
-                        new ParallelAction(
-                                slides.slideTopBar()
-                        ),
-                        slides.slideTopBarClip(),
-                        //new SleepAction(0.25),
-                        claw.open(),
-                        //new SleepAction(0.2),
-                        slides.retract(),
-                        claw.close(),
-                        new ParallelAction(
-                                slides.slideTopBar()
-                        ),
-                        slides.slideTopBarClip(),
-                        //new SleepAction(0.25),
-                        claw.open(),
-                        //new SleepAction(0.2),
-                        slides.retract()
-
-
-                )
+//                new SequentialAction(
+//                        slides.slideTopBar(),
+//                        slides.slideTopBarClip(),
+//                        new SleepAction(0.25),
+//                        claw.open(),
+//                        new SleepAction(0.2),
+//                        slides.retract(),
+//                        new ParallelAction(
+//
+//                                extendo.extend()
+//                        ),
+//                        intake.flat(),
+//                        new ParallelAction(
+//                                extendo.retract(),
+//                                intake.flop()
+//                        ),
+//                        claw.close(),
+//                        new ParallelAction(
+//                                slides.slideTopBar()
+//                        ),
+//                        slides.slideTopBarClip(),
+//                        new SleepAction(0.25),
+//                        claw.open(),
+//                        new SleepAction(0.2),
+//                        slides.retract(),
+//                        claw.close(),
+//                        new ParallelAction(
+//                                slides.slideTopBar()
+//                        ),
+//                        slides.slideTopBarClip(),
+//                        //new SleepAction(0.25),
+//                        claw.open(),
+//                        //new SleepAction(0.2),
+//                        slides.retract(),
+//                        claw.close(),
+//                        new ParallelAction(
+//                                slides.slideTopBar()
+//                        ),
+//                        slides.slideTopBarClip(),
+//                        //new SleepAction(0.25),
+//                        claw.open(),
+//                        //new SleepAction(0.2),
+//                        slides.retract()
+//
+//
+//                )
 
         ));
 
