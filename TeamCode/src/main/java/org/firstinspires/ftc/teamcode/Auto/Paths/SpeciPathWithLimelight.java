c128e68fccc76d4d5cea21e5b3d4dda262c742cc
Daniel Yeh <somethings8596@gmail.com>
2025-02-13 20:39:06 -0800

    Added onto Limelight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Paths/SpeciPathWithLimelight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Paths/SpeciPathWithLimelight.java
@@ -2,9 +2,11 @@
 
 package org.firstinspires.ftc.teamcode.Auto.Paths;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.linearOpMode;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
@@ -19,25 +21,46 @@ import org.firstinspires.ftc.teamcode.Auto.Vision.Limelight;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.MecanumDriveSWBot;
 
-@Disabled
+import java.util.concurrent.Callable;
+
 @Autonomous
 public class SpeciPathWithLimelight extends LinearOpMode{
+
+
     @Override
     public void runOpMode() {
         Limelight limelight = new Limelight(hardwareMap);
         Pose2d StartPose1 = new Pose2d(0,0, Math.toRadians(0));
         MecanumDriveSWBot drive = new MecanumDriveSWBot(hardwareMap, StartPose1);
 
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        telemetry.clear();
 
 
         waitForStart();
-        Actions.runBlocking(limelight.align(drive));
-        new Vector2d(1,1);
 
+        while(opModeIsActive()) {
+            Actions.runBlocking(
+                    limelight.align(drive, telemetry)
+                    // extendo.runToDistance(limelight.getDistance(cameraHeight); 13 in this instance? Implement once tuned
+                    // extendo.retract();
+                    // back up
+            );
+            telemetry.addData("Test", limelight.getDistance(13));
+            telemetry.update();
 
+            double tx;
 
+        /*while (true){
 
+            tx = limelight.getTx();
+            telemetry.addData("tx", tx);
+            telemetry.update();
+        }*/
+
+            //new Vector2d(1,1);
 
         }
+    }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Vision/Limelight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Vision/Limelight.java
@@ -1,18 +1,31 @@
 
 package org.firstinspires.ftc.teamcode.Auto.Vision;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.opMode;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+import android.telecom.Call;
 
 import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.ProfileAccelConstraint;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -24,11 +37,17 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.MecanumDriveSWBot;
+import org.firstinspires.ftc.teamcode.mechanisms.Claw;
+import org.firstinspires.ftc.teamcode.mechanisms.ExtendoV2;
+import org.firstinspires.ftc.teamcode.mechanisms.Intaker;
+import org.firstinspires.ftc.teamcode.mechanisms.SlidesV3;
 
 import java.nio.file.Paths;
 import java.util.List;
+import java.lang.Math;
+import java.util.concurrent.Callable;
 
-public class Limelight {
+public class Limelight{
     public Limelight3A limelight;
 
 
@@ -43,38 +62,52 @@ public class Limelight {
 //need limeight to move until degree = 0
     public class Align implements Action {
         private MecanumDriveSWBot drive;
+        private Telemetry telem;
 
-        public Align(MecanumDriveSWBot drive) {
+        public Align(MecanumDriveSWBot drive, Telemetry telemetry) {
             this.drive = drive;
+            this.telem = telemetry;
 
         }
 
+
         @Override
         public boolean run(@NonNull TelemetryPacket packet) {
+            boolean oop;
+
+            oop = true;
+
+
+
             double tx = getTx();
-            if (tx > 15) {
+
+            telem.addData("tx", tx);
+            telem.update();
+
+            if (tx > 2) {
                 drive.setDrivePowers(new PoseVelocity2d(
                         new Vector2d(0, 0.3),
                         0));
-            }
-
-            else if (tx < -15) {
+            } else if (tx < -2) {
                 drive.setDrivePowers(new PoseVelocity2d(
                         new Vector2d(0, -0.3),
                         0
                 ));
-            }
+            } else {
 
-            else {
-                return false;
+                double distance = getDistance(13);
+                telem.addData("Distance", distance);
+                drive.setDrivePowers(new PoseVelocity2d(
+                                new Vector2d(0,0), 0)
+                );
+                return true; // Change to false once done testing and tuning
             }
-
             return true;
         }
     }
 
-    public Action align(MecanumDriveSWBot drive) {
-        return new Align(drive);
+    public Action align(MecanumDriveSWBot drive, Telemetry telemetry) {
+        return new Align(drive, telemetry);
 
     }
 
@@ -95,6 +128,30 @@ public class Limelight {
         return tx;
     }
 
+    public double getDistance(double cameraHeight){
+        LLResult result = limelight.getLatestResult();
+
+        double tx;
+        double ty;
+
+        if (result != null && result.isValid()){
+            tx = result.getTx();
+            ty = result.getTy();
+
+            if (tx <= 15 & tx >= -15){
+
+                double verticalAngle = Math.toRadians(ty);
+                return cameraHeight * Math.tan(verticalAngle);
+
+            }
+
+            return 0;
+
+        } else{
+            return 0;
+        }
+    }
+
     public double StrafeAmount() {
         return 0;
     }
@@ -104,9 +161,13 @@ public class Limelight {
     //put into method/atcition
     //then strafe
 
+    public int testW(){
+        return 21;
+    }
+
+
+
 }
-
-
 
 
 
